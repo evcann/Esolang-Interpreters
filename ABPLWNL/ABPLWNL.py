ff081774ab10b60eaea5b7b09d717f9739898289
evcann <92185959+evcann@users.noreply.github.com>
2024-12-24 22:59:47 -0500

    Add ABPLWNL

---
+++ ABPLWNL/ABPLWNL.py
@@ -0,0 +1,37 @@
+instring = input("Enter code: ")
+commands = list(instring)
+
+a, b = 0, 0
+
+for i, command in enumerate(commands):
+    match (command):
+        case '1':
+            a += 1
+        case '2':
+            if a != 0:
+                a -= 1
+        case '3':
+            a, b = b, a
+        case '4':
+            if b != 0:
+                b -= 1
+                a += 1
+        case '5':
+            a += b
+            b = 0
+        case '6':
+            print(b, end='')
+        case '7':
+            print(chr(b), end='')
+        case '8':
+            a = 0
+        case '9':
+            try: 
+                b = int(input(':'))
+            except ValueError:
+                print("\nError: invalid literal for int with base 10")
+                print(f"Command: {command}, index: {i}")
+                exit()
+        case 'a':
+            if not a:
+                commands.pop(i+1)
