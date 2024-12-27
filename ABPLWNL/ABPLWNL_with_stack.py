7961c7c3604137bba13e1c87fd4c8f7347faf834
evcann <92185959+evcann@users.noreply.github.com>
2024-12-26 21:07:27 -0500

    Added ABPLWNL with stack interpreter

---
+++ ABPLWNL/ABPLWNL_with_stack.py
@@ -0,0 +1,57 @@
+def ABPLWNL_input(program):
+    commands = list(program)
+
+    a, b = 0, 0
+    stack = []
+
+    for i, command in enumerate(commands):
+        match (command):
+            case '1':
+                a += 1
+            case '2':
+                if a != 0:
+                    a -= 1
+            case '3':
+                a, b = b, a
+            case '4':
+                if b != 0:
+                    b -= 1
+                    a += 1
+            case '5':
+                a += b
+                b = 0
+            case '6':
+                print(b, end='')
+            case '7':
+                print(chr(b), end='')
+            case '8':
+                a = 0
+            case '9':
+                try: 
+                    b = int(input(':'))
+                except ValueError:
+                    print("\nError: invalid literal for int with base 10")
+                    print(f"Command: {command}, index: {i}")
+                    exit()
+            case 'a':
+                if not a:
+                    commands.pop(i+1)
+            case 't':
+                stack.append(a)
+            case 'u':
+                if len(stack) > 0:
+                    a = stack.pop()
+                else:
+                    print(f"Stack underflow error.")
+                    print(f"Command: {command}, index: {i}")
+                    exit()
+            case 'e':
+                exit()
+            case _:
+                print(f"\nInvalid command {command} at index {i}.")
+                exit()
+
+if __name__ == "__main__":
+    program_input = input("Enter code:")
+    ABPLWNL_input(program_input)
+    print('\n')
